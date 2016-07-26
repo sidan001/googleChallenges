b1c3016a9f64d93597b9da16060d716f4eb8eff9
sidan001 <ljl052@foxmail.com>
2016-07-26 17:15:42 +0800

    zombit_intection

--- a_pirate_walks_into_a_bar/solution.java
+++ a_pirate_walks_into_a_bar/solution.java
@@ -1,6 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Answer{
 	public static int answer(int[] numbers){
-		//Your code goes here
+		List<Integer> pirates = new ArrayList<Integer>();
+		int i = 0;
+		while (true) {
+
+			if (pirates.contains(i)) {
+				return pirates.size() - pirates.indexOf(i);
+			}
+			pirates.add(i);
+
+			i = numbers[i];
+		}
 	}
 }
 
---
+++ zombit_intection/solution.java
@@ -0,0 +1,7 @@
+public​ ​class​ ​Answer​ ​{​ ​​ ​​ ​
+​ ​​ ​​ ​​ ​public​ ​static​ ​int[][]​ ​answer(int[][]​ ​population,​ ​int​ ​x,​ ​int​ ​y,​ ​int​ ​strength)​ ​{​ ​
+
+​ ​​ ​​ ​​ ​​ ​​ ​​ ​​ ​//​ ​Your​ ​code​ ​goes​ ​here.
+
+​ ​​ ​​ ​​ ​}​ ​
+}
